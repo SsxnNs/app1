5a1aa51bd4abe4a999b2eacef945b0dc455d7d60
SsxnN <sunsurod@gmail.com>
2025-08-04 10:01:42 +0700

    add button OK
    button OK

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import React from 'react';
-import './style.css';
-import { Header, Content, Footer } from './func-components';
-import Calendar from './class-components'
-
+import React from 'react'
+import Button from './class-components'
 
 function App() {
-  const divstyle = {
-    color: 'red',
-    backgroundColor: 'blue',
-    fontSize: 'larger',
-    padding: '3px'
-  };
-
-  return (
-    <>
-      <Header />
-      <p><center><Calendar/></center></p>
-      <Content />
-      <Footer />
-    </>
-  );
+  return <Button/>
 }
 
 export default App;
