2d213a2698370d9d2014bdddef3fe4e1b394da36
SsxnN <sunsurod@gmail.com>
2025-09-08 10:59:37 +0700

    Router
    Router

---
+++ src/router1.js
@@ -0,0 +1,16 @@
+import React from "react"
+import {BrowserRouter, Link} from 'react-router-dom'
+
+export default function Router1(){
+    const navStyle = {
+        backgroundColor: '#ced',
+        padding: '7',
+        textAlign: 'center',
+    }
+
+    const linkStyle = {
+        display: 'inline-block',
+        color : 'blue',
+        textDecoration: 'none',
+    }
+}
